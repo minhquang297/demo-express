15f48a2309faab4b89a243f28bad9bd68304430f
Minh Quang <minhquang2971999@gmail.com>
2019-05-28 20:21:23 +0700

    html to pug

--- 1.js
+++ 1.js
@@ -2,6 +2,29 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'));
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+app.get('/', function (req, res) {
+    res.render('index', {
+        name: 'Quang'
+    });
+})
+
+app.get('/users', function (req, res) {
+    res.render('users/index', {
+        users: [
+            {
+                id: 1,
+                name: 'Quang'
+            },
+            {
+                id: 2,
+                name: 'Ha'
+            }
+        ]
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
