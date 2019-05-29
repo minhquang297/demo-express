8a1921861ec4eabde2ae1cf88de8672cf7b29de5
Minh Quang <minhquang2971999@gmail.com>
2019-05-29 11:39:26 +0700

    Controller (MVC)

--- router/user.router.js
+++ router/user.router.js
@@ -1,45 +1,20 @@
 var express = require('express');
-var shortid = require('shortid');
 
-var db = require('../db');
+var controller = require('../controller/user.controller');
+
 
 var router = express.Router();
 
-router.get('/', function (req, res) {
-    res.render('users/index', {
-        users: db.get('users').value()
-    });
-});
+router.get('/', controller.index)
 
-router.get('/search', function (req, res) {
-    var q = req.query.q
-    var matchedUsers = users.filter(function (user) {
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-        // Hàm indexOf sẽ tìm kiếm một phần tử trong mảng dựa vào giá trị của 
-        // phần tử, hàm sẽ trả về vị trị( khóa) của phần tử nếu tìm thấy và 
-        // trả về -1 nếu không tìm thấy.
-    })
-    res.render('users/index', {
-        users: matchedUsers
-    });
-})
+router.get('/search', controller.search)
 
 router.get('/create', function (req, res) {
     res.render('users/create');
 });
 
-router.get('/:id', function (req, res) {
-    var id = req.params.id;
-    var user = db.get('users').find({ id: id }).value();
-    res.render('users/view', {
-        user: user
-    })
-});
+router.get('/:id', controller.get);
 
-router.post('/create', function (req, res) {
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect('/users')
-});
+router.post('/create', controller.postCreate);
 
 module.exports = router;
