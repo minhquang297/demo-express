3a415fef2b3d76bdb75018106248229238872b17
Minh Quang <minhquang2971999@gmail.com>
2019-05-29 11:25:06 +0700

    Express Router

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var userRouter = require('./router/user.router');
 
 var port = 3000;
-var shortid = require('shortid');
-var low = require('lowdb');
-var FileSync = require('lowdb/adapters/FileSync');
-var adapter = new FileSync('db.json');
-db = low(adapter);
+
+var app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-// Set some defaults
-db.defaults({ users: [] })
-    .write()
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', function (req, res) {
     res.render('index', {
@@ -25,43 +19,7 @@ app.get('/', function (req, res) {
     });
 })
 
-app.get('/users', function (req, res) {
-    res.render('users/index', {
-        users: db.get('users').value()
-    });
-});
-
-app.get('/users/search', function (req, res) {
-    var q = req.query.q
-    var matchedUsers = users.filter(function (user) {
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-        // Hàm indexOf sẽ tìm kiếm một phần tử trong mảng dựa vào giá trị của 
-        // phần tử, hàm sẽ trả về vị trị( khóa) của phần tử nếu tìm thấy và 
-        // trả về -1 nếu không tìm thấy.
-    })
-    res.render('users/index', {
-        users: matchedUsers
-    });
-})
-
-app.get('/users/create', function (req, res) {
-    res.render('users/create');
-});
-
-app.get('/users/:id', function (req, res) {
-    var id = req.params.id;
-    var user = db.get('users').find({ id: id }).value();
-    res.render('users/view', {
-        user: user
-    })
-});
-
-app.post('/users/create', function (req, res) {
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect('/users')
-});
-
+app.use('/users', userRouter);
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
