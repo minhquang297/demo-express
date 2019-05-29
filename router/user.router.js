ba627b0a932bcedfa8f14df53355221dcbd8ae80
Minh Quang <minhquang2971999@gmail.com>
2019-05-29 21:10:42 +0700

    merge branch demo

--- router/user.router.js
+++ router/user.router.js
@@ -7,22 +7,7 @@ var router = express.Router();
 
 router.get('/', controller.index)
 
-<<<<<<< HEAD
-router.get('/search', function (req, res) {
-    var q = req.query.q
-    var matchedUsers = db.get('users').value().filter(function (user) {
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-        // Hàm indexOf sẽ tìm kiếm một phần tử trong mảng dựa vào giá trị của 
-        // phần tử, hàm sẽ trả về vị trị( khóa) của phần tử nếu tìm thấy và 
-        // trả về -1 nếu không tìm thấy.
-    })
-    res.render('users/index', {
-        users: matchedUsers
-    });
-})
-=======
 router.get('/search', controller.search)
->>>>>>> demo
 
 router.get('/create', function (req, res) {
     res.render('users/create');
